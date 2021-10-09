a11b21440a7f55fc70097197fc16590dbe535144
sum16 <godasper@gmail.com>
2021-10-09 18:16:21 +0900

    メソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_many :user_scores
   has_one :rank
+
+  def total_score
+    @total_score ||= user_scores.sum(&:score)
+  end
+
 end
+
+# インスタンス変数 ||=を使用すると計算結果のキャッシュを保持することができ、メソッドが2回目以降に呼び出されたときの速度向上が期待できる
