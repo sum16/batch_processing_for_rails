b577d81d09b7ccd2416a198de2375220a8e55b66
sum16 <godasper@gmail.com>
2021-10-10 18:11:30 +0900

    エラーを修正

--- app/models/rank_order_maker.rb
+++ app/models/rank_order_maker.rb
@@ -6,19 +6,19 @@ class RankOrderMaker
     # 変数の初期化
     # rankは順位を格納する変数で最初は1からはじめる previous_scoreはrankの変数を更新すべきかを判定するために使用
     rank = 1
-    privious_score = 0
+    previous_score = 0
 
     # 全ユーザーの中からスコアを獲得しているユーザーを選択し、合計スコアの高い順に並び替え
     users_sorted_by_score.each do |user|
       # 合計スコアが変わった場合は順位を更新
-      rank += 1 if user.total_score < privious_score
+      rank += 1 if user.total_score < previous_score
 
       # app/models/ranks_updater.rb の create_ranksに書いた
       # Rank.create(user_id: user.id, rank: rank, score: user.total_score) を実行
-      yield(user,rank)
+      yield(user, rank)
       
       # previous_scoreを更新して、次の順位更新に備える
-      privious_score = user.total_score
+      previous_score = user.total_score
     end
   end
 
@@ -27,7 +27,7 @@ class RankOrderMaker
   def users_sorted_by_score
     # selectで絞り込み,sort_byで降順に
     User.all
-    .select{ |user| user.total_score.nonzero? }
+    .select { |user| user.total_score.nonzero? }
     .sort_by { |user| user.total_score * -1 }
 
     #total_score = Userクラスのインスタンスメソッド
